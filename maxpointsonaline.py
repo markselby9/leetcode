2dc5fac58ad200d5fe0fb93311ce0f124e0996e6
markselby <markselby@ubuntu.ubuntu-domain>
2014-06-17 16:59:57 +0800

    maxpointsolved

--- maxpointsonaline.py
+++ maxpointsonaline.py
@@ -1,31 +1,40 @@
+# Solution: use python dictionary to keep the result, pay attention to the same points
+# and the slope should be float
+
 class Solution:
     # @param points, a list of Points
     # @return an integer
     def maxPoints(self, points):
-	tmp = []
-	l = len(points)
-	for i in range(l):
-	    for j in range(i, l):
+	length = len(points)
+	if length < 2:
+	    return length
+	result = 0
+	for i in range(length):
+	    samepoint = 0 
+	    slope={'inf':0}
+	    for j in range(length):
 		p = points[i]
 		q = points[j]
 		if (p.x == q.x):
-		    rec = -1
+		    if (p.y == q.y):
+			samepoint+=1
+		    else:
+			slope['inf']+=1
 		else:
-		    rec = (p.y - q.y) / (p.x - q.x)
-		tmp.append(rec)
-	tmp.sort()
-	print tmp 
-	
+		    it = 1.0 * (p.y - q.y) / (p.x - q.x)
+		    if it in slope: slope[it]+=1
+		    else: slope[it]=1
+	    result = max(result, max(slope.values())+samepoint)
+#            print slope
+	return result
+
 
 class Point:
     def __init__(self, a=0, b=0):
 	self.x = a
 	self.y = b
 
-a = Point(1,1)
-b = Point(1,0)
-c = Point(0,0)
-points = [a,b,c]
+points = [Point(84,250),Point(0,0),Point(1,0),Point(0,-70),Point(0,-70),Point(1,-1),Point(21,10),Point(42,90),Point(-42,-230)]
 s = Solution()
 print s.maxPoints(points)
 
