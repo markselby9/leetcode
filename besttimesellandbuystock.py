a1cd3c8f8757c08d78234e001ce67685aa01bb85
markselby <markselbyfcy@gmail.com>
2015-06-07 16:06:02 +0800

    besttime2

--- besttimesellandbuystock.py
+++ besttimesellandbuystock.py
@@ -5,18 +5,19 @@ class Solution:
     # @return an integer
     def maxProfit(self, prices):
         result = 0
-	size = len(prices)
-	if size < 1: return 0
-	for i in range(size-1):
-	    prices[i] = prices[i+1]-prices[i]
-	prices[size-1] = 0
+        size = len(prices)
+        if size < 1: return 0
+        for i in range(size - 1):
+            prices[i] = prices[i + 1] - prices[i]
+        prices[size - 1] = 0
         segsum = 0
-	for i in range(size):
-	    segsum += prices[i]
-	    if segsum < 0: segsum = 0
-	    if result < segsum: result = segsum
-	return result
+        for i in range(size):
+            segsum += prices[i]
+            if segsum < 0: segsum = 0
+            if result < segsum: result = segsum
+        return result
 
-prices = [7,-3,4,0,1,2,3,4,5,6,7]
+
+prices = [7, -3, 4, 0, 1, 2, 3, 4, 5, 6, 7]
 c = Solution()
 print c.maxProfit(prices)	
